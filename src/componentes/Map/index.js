27100d200421638d5968f6beec89681d2dfac9ad
Bartek <bartlomiej.perucki@solarwinds.cloud>
2019-04-28 12:25:38 +0200

    Add currentBridge marker color

--- src/componentes/Map/index.js
+++ src/componentes/Map/index.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
-import BridgesContext from '../../contexts/bridges-context';
 import { Map as GoogleMap, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import BridgesContext from '../../contexts/bridges-context';
 import BridgeInfo from './BridgeInfo';
+
+const GREEN_MARKER = "https://mt.google.com/vt/icon?psize=30&font=fonts/arialuni_t.ttf&color=ff304C13&name=icons/spotlight/spotlight-waypoint-a.png&ax=43&ay=48&text=%E2%80%A2";
+const RED_MARKER = "https://mt.googleapis.com/vt/icon/name=icons/spotlight/spotlight-poi.png&scale=1";
 
 const Map = (props) => {
   const googleMap = useRef();
@@ -10,6 +13,7 @@ const Map = (props) => {
   const [ bridges, _ ] = useContext(BridgesContext);
   const [ marker, setMarker ] = useState();  
   const [ currentBridge, setCurrentBridge ] = useState(defaultCurrentBridge);
+  const [ zoom, setZoom ] = useState(currentBridge ? 10 : 5);
 
   const onMarkerClick = (marker, bridge) => {
     setMarker(marker);
@@ -17,25 +21,39 @@ const Map = (props) => {
   }
 
   const renderMarkers = () => {
-    return bridges.map(bridge => (
-      <Marker
-        onClick={(_, marker, e) => onMarkerClick(marker, bridge)}
-        key={bridge.id}
-        position={{
-          lat: 47 + bridge.country.length,
-          lng: 10 + bridge.name.length
-        }}
-      />
-    ))
+    return bridges.map(bridge => {
+      
+      // if zoom smaller than X do not render small bridges
+      return (
+        <Marker
+          onClick={(_, marker, e) => onMarkerClick(marker, bridge)}
+          key={bridge.id}
+          position={{
+            lat: 47 + bridge.country.length,
+            lng: 10 + bridge.name.length
+          }}
+          icon={{
+            url: bridge === currentBridge ? GREEN_MARKER : RED_MARKER
+          }}
+        />
+      )
+    })
   }
+
+  useEffect(() => {
+    if(googleMap.current) {
+      googleMap.current.map.addListener('zoom_changed', (e) => {
+        setZoom(googleMap.current.map.getZoom());
+      })
+    }
+  }, [])
 
   return (
     <GoogleMap
       ref={googleMap}
       google={props.google}
-      zoom={currentBridge ? 10 : 5}
+      zoom={zoom}
       style={{ width: '100%', heigth: '100%'}}
-      onPositionChanged={() => console.log('aaa')}
       initialCenter={{
         lat: currentBridge ? 47 + currentBridge.country.length : 47,
         lng: currentBridge ? 10 + currentBridge.name.length : 10,
