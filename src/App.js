18316a5c30a459c4f6c759d11b1de56e9da81ea2
Bartek <bartlomiej.perucki@solarwinds.cloud>
2019-05-17 20:21:57 +0200

    Use container network

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import BridgesContext from './contexts/bridges-context';
 import bridgesReducer, { INITIAL_STATE } from './reducers/bridges-reducer';
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://0.0.0.0:3000/graphql"}),
+  link: new HttpLink({ uri: "http://api:3000/graphql"}),
   cache: new InMemoryCache(),
 });
 
